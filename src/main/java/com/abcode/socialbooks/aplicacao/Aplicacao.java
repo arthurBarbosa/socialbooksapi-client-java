7d487916aec73cfd900208fe248ccaa5d4fccb10
Arthur Barbosa <arthurbruno03@gmail.com>
2020-01-06 12:28:48 -0400

    listando e salvando livro

--- src/main/java/com/abcode/socialbooks/aplicacao/Aplicacao.java
+++ src/main/java/com/abcode/socialbooks/aplicacao/Aplicacao.java
@@ -1,27 +1,36 @@
 package com.abcode.socialbooks.aplicacao;
 
-import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
+import com.abcode.socialbooks.client.LivrosClient;
 import com.abcode.socialbooks.client.domain.Livro;
 
 public class Aplicacao {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		LivrosClient cliente = new LivrosClient();
+		
+		List<Livro> listaLivros = cliente.listar();
 
-		RestTemplate restTemplate = new RestTemplate();
-
-		RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/livros"))
-				.header("Authorization", "Basic dXNlcjpzZW5oYQ==").build();
-
-		ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
-
-		for (Livro livro : response.getBody()) {
+		for (Livro livro : listaLivros) {
 			System.out.println("Livro " + livro.getNome());
 
 		}
+		
+		Livro livro = new Livro();
+		livro.setNome("Git passo a passo");
+		livro.setEditora("Abcode");
+		
+		SimpleDateFormat publicacao = new SimpleDateFormat("dd/MM/yyyy");
+		
+		livro.setPublicacao(publicacao.parse("06/01/2020"));
+		livro.setResumo("este livro aborda tecnicas de desenvolvimento de API");
+		
+		String localizacao = cliente.salvar(livro);
+		
+		System.out.println("URI do livro salvo " + localizacao);
 	}
 }
